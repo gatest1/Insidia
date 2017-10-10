ae93ccb1b55cf31f6f9bc0cb6d0f704be328cf1f
Unknown <jacobrressler@gmail.com>
2017-10-10 14:25:15 -0600

    add stuff
    added

---
+++ InsidiaGame/Assets/Scripts/AI/jacob/move.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class move : MonoBehaviour {
+
+    public float speed;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        var x = Input.GetAxisRaw("Horizontal") * speed;
+        var y = Input.GetAxisRaw("Vertical") * speed;
+        transform.Translate(0, 0, y);
+        transform.Rotate(0, x, 0);
+	}
+}

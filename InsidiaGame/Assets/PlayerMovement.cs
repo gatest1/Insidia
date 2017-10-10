02d3c01bd0f83838692029ac2eed60c8c1a83bf7
Unknown <bbqdpickles@gmail.com>
2017-10-10 13:22:12 -0600

    Convert and Follow scripts

---
+++ InsidiaGame/Assets/PlayerMovement.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float moveSpeed = 8.0f;
+    public float turnSpeed = 30.0f;
+    [Range(0, 50)]
+    public float gravity = 10f;
+    public float jumpHeight = 10f;
+    [SerializeField]
+    private float _yVelocity = 0f;
+    private CharacterController _charController;
+
+    public string moveHorz = "Horizontal";
+    public string moveVert = "Vertical";
+    public string jump = "Jump";
+
+    // Use this for initialization
+    void Start()
+    {
+        _charController = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var y = Input.GetAxis(moveHorz) * Time.deltaTime * turnSpeed;
+        var z = Input.GetAxis(moveVert) * Time.deltaTime * moveSpeed;
+        if (Input.GetButtonDown(jump) && IsTouchingGround())
+            _yVelocity += Mathf.Sqrt(2 * gravity * jumpHeight);
+
+
+        // Rotates/turns the player
+        transform.Rotate(0, y, 0);
+
+        Vector3 velocity = Vector3.zero; //vector3(0,0,0)
+        velocity.z = z;
+
+        // Take accel of grav per second and add to yVelocity
+        _yVelocity -= gravity * Time.deltaTime;
+        // Assign speed to velocity.y part of the vector.
+        velocity.y = _yVelocity * Time.deltaTime;
+        velocity = transform.rotation * velocity;
+
+        // Moves character via char controller
+        _charController.Move(velocity);
+
+        if (IsTouchingGround())
+            _yVelocity = 0;
+
+    }
+
+    private bool IsTouchingGround()
+    {
+        return (_charController.collisionFlags & CollisionFlags.Below) != 0;
+    } 
+}
---
+++ InsidiaGame/Assets/Scripts/Util/ReadOnlyAttribute.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// Use this to make a variable non-editable from the inspector, but still show it's value.<para>
+/// Created by Christian Clark</para>
+/// </summary>
+public class ReadOnlyAttribute : PropertyAttribute
+{
+
+}
